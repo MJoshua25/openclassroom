0ff68acffd8eb395bb50b6bcce9c5ae50c5bb093
Yao Josué <jyao2000@yahoo.fr>
2020-05-12 14:03:27 +0200

    scanner

--- tponeCelsius/src/com/devmax/Main.java
+++ tponeCelsius/src/com/devmax/Main.java
@@ -1,8 +1,15 @@
 package com.devmax;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        Definissons notre scanner
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+    public static double arrondi(double A, int B) {
+        return (double) ( (int) (A * Math.pow(10, B) + .5)) / Math.pow(10, B);
     }
 }
